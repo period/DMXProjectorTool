a8629a0acb37700cfd532441b647998f3593c79a
Thomas <chearful@chearfulcraft.org>
2020-12-09 19:23:36 +0000

    Remove multi video capabilities

--- index.js
+++ index.js
@@ -7,16 +7,9 @@ const WebSocket = require("ws");
 const http = require("http");
 const fs = require("fs");
 
-const videoFiles = Object.values(config.videos);
-console.log(videoFiles);
-
 var server = http.createServer((req, res) => {
-  if(req.url == "/config.json") {
-    res.writeHead(200, {"Content-Type": "application/json"});
-    res.end(JSON.stringify(config));
-  }
-  else if(videoFiles.includes(req.url.replace("/", ""))) {
-    fs.readFile("./videos/" + req.url.replace("/", ""), (err, data) => {
+  if(req.url == "/video.mp4") {
+    fs.readFile("./videos/video.mp4", (err, data) => {
       if(err) {
         res.writeHead(500);
         return res.end("Error");
@@ -42,11 +35,8 @@ server.listen(config.http_port);
 const ws = new WebSocket.Server({server});
 
 let receiver = new acnReceiver.Universe(1);
-let last_received_data = -1;
 receiver.on("packet", (packet) => {
-  if(packet.getSlots()[0] == last_received_data) return;
-  last_received_data = packet.getSlots()[0]
     ws.clients.forEach((client) => {
-        if (client.readyState === WebSocket.OPEN) client.send(data);
+        if (client.readyState === WebSocket.OPEN) client.send(packet.getSlots()[0]);
     });
 });
